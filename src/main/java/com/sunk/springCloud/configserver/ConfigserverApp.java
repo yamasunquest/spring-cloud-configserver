9494d27827cba00bf130f8bddf56f59b09914219
sunquest <sunquest@sohu.com>
2016-03-04 11:10:34 +0800

    init

---
+++ src/main/java/com/sunk/springCloud/configserver/ConfigserverApp.java
@@ -0,0 +1,12 @@
+package com.sunk.springCloud.configserver;
+
+import org.springframework.boot.SpringApplication;
+
+
+public class ConfigserverApp 
+{
+    public static void main( String[] args )
+    {
+		SpringApplication.run(ConfigserverApp.class, args);
+    }
+}
